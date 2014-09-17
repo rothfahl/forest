71bdcafe5c0c290f4b80edcedf7989ba98d774a3
rothfahl <simon.rothfahl@gmail.com>
2014-09-17 20:06:58 +0200

    beautify sliders + add touch capabalities

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+    <script src="lib/jquery.ui.touch-punch.min.js"></script>
     <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
     <style>
       body {
@@ -14,23 +15,33 @@
         top: 0; left: 0; right: 0;
         border-bottom: solid grey 1px;
       }
+      #sliders {
+        width: 50%;
+        margin: 0 auto;
+      }
+      #sliders p {
+        margin: 10px 0 5px 0;
+      }
+      label {
+        font-family: monospace;
+      }
     </style>
     
   </head>
   <body>
-    <div>
-    <canvas id="canvas"></canvas>
+    <div><canvas id="canvas"></canvas></div>
+    <div id="sliders">
+      <p>
+        <label for="number-of-trees">Number of trees: <span class="value" id="value-number-of-trees"></label>
+        <input type="text" id="number-of-trees" style="border:0; color:#f6931f; font-weight:bold;">
+      </p>
+      <div id="slider-number-of-trees"></div>
+      <p>
+        <label for="delay">Delay (ms): <span class="value" id="value-delay"></label>
+        <input type="text" id="delay" style="border:0; color:#f6931f; font-weight:bold;">
+      </p>
+      <div id="slider-delay"></div>
     </div>
-    <p>
-      <label for="number-of-trees">Number of trees: <span class="value" id="value-number-of-trees"></label>
-      <input type="text" id="number-of-trees" style="border:0; color:#f6931f; font-weight:bold;">
-    </p>
-    <div id="slider-number-of-trees"></div>
-    <p>
-      <label for="delay">Delay (ms): <span class="value" id="value-delay"></label>
-      <input type="text" id="delay" style="border:0; color:#f6931f; font-weight:bold;">
-    </p>
-    <div id="slider-delay"></div>
   </body>
   <script type="text/javascript" src="tree.js"></script>
   <script type="text/javascript">
@@ -63,7 +74,7 @@
         $("#slider-delay").slider({
           value: tree.defaults.delay,
           min: 0,
-          max: 2000,
+          max: 1000,
           step: 100,
           slide: function( event, ui ) {
             console.log('delay changed');
